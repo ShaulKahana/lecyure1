238366eef4cc981eec656f99ef77979320981f58
Shaul Kahane <35196468+shaolkahna@users.noreply.github.com>
2022-12-04 21:41:25 +0200

    write and read

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-let x = 0;
-while (x<100) {
-    console.log("x",x++);
+import {writeFile, readFile } from 'node:fs';
+import { exit } from 'node:process';
+
+const me = {
+    firstName: "Shaul",
+    lastName: "kahana",
+    age: 37
 }
+
+writeFile("./me.json",JSON.stringify(me), (err) => {
+    if(arr) {
+        console.log(err);
+        exit(1);
+    }
+    readFile("./me.json",(err, data) => {
+        if(arr) {
+            console.error(err);
+            exit(2);
+        }
+        console.log("$s",data)
+            exit(0);
+    })
+    
+})
